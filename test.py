17b6a0e81562665473a050ebb62d8fbde7844fcb
Mega-Gorilla <hahahadesu@gmail.com>
2025-02-14 05:45:54 +0000

    ログインテストまで

--- test.py
+++ test.py
@@ -1,2 +1,18 @@
-# hello.py
-print("Hello from Docker!")
+import os
+import socket
+from javascript import require, On, Once, AsyncTask, once, off
+
+mineflayer = require('mineflayer')
+# ローカルサーバーの設定
+bot = mineflayer.createBot({
+    'username': 'simple-bot',
+    'host': '192.168.100.24',
+    'port': 30000,
+    'version': '1.21.4',
+    'hideErrors': False
+})
+
+# ログインイベント
+@On(bot,'login')
+def login(this):
+    print('ログインしました')
